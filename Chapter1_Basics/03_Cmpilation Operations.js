a9e37c1b888a99cf26bbdd4b9f8c768e6823f1c9
Rohit Kulkarni <kulkarni.rohit505@gmail.com>
2026-06-16 10:45:29 +0530

    Compilation and JS Comands

---
+++ Chapter1_Basics/03_Cmpilation Operations.js
@@ -0,0 +1,21 @@
+// This is simple code can be executed with intrpreter (No Turbo Fan is need)
+console.log("Hey this is future opr");
+let num = 10;
+
+
+// This needs optimization because this is bigger operation 
+for (let i = 0; i < 10 ; i++) {
+console.log("Hi this is "+i+" itr"); 
+}
+
+/*
+How to print byte code?
+node --print-bytecode Chapter1_Basics/03_Cmpilation Operations.js
+Syntax ->
+node --print-bytecode <FolderName>/<FileName><Extention>
+
+
+
+*/
+
+
---
+++ Chapter1_Basics/04_JS_Commands.js
@@ -0,0 +1,2 @@
+console.log(process.platform);
+console.log("Node Version :", process.version)
