a73a77b34ad40cc4160a2cc859da58e983838875
Rohit Kulkarni <kulkarni.rohit505@gmail.com>
2026-06-17 22:59:28 +0530

    Array Extras

---
+++ Chapter11_Arrays/94_ArrayConcat.js
@@ -0,0 +1,26 @@
+let a = [1, 2];
+let b = [3, 4];
+let x = [5, 6];
+let y = [7, 8];
+
+
+// // let c = a + b;
+// let c = a.concat(b);
+// console.log(c);
+
+// // spread (modern way) - concatenation. (...)
+// let d = [...a, ...b];
+// console.log(d);
+
+
+
+// // Join 
+// let s = ["pass", "fail", "skip"].join("|");
+// console.log(s);
+
+
+let ans = [...x, ...y];
+console.log(ans);
+let ansNew = ans.join("-->");
+console.log(ansNew);
+console.log(ans);
